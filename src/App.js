8811cbc5d2bcd6a7e5fda7e94736d7e429a47e23
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-11-05 10:42:33 -0400

    use endpoint to connect to star wars api and obtain characters data

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  const [characters, setCharacters] = useState([]);
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+  useEffect(() => {
+    axios.get(`https://swapi.dev/api/films`)
+      .then(resp => {
+        console.log(resp.data.results);
+      })
+      .catch(err => {
+        console.error(err);
+      })
+  }, []);
 
   return (
     <div className="App">
