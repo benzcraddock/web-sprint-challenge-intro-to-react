cbcd9a1535b7a496f2f667c2cdc9b81427592a45
Benjamin Craddock <benjamincraddock@benjamins-mbp.lan>
2021-11-05 12:31:09 -0400

    achieved mvp and added style using styled components

--- src/components/CharacterPrint.js
+++ src/components/CharacterPrint.js
@@ -1,12 +1,30 @@
 import React from 'react';
 import styled from 'styled-components';
 
+const StyledCharacterPrint = styled.div`
+    border: 2px solid green;
+    margin: 15px;
+    width: 75%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    color: #98B9F2;
+    background-color: #141B41;
+
+    h3 {
+        font-size: 20px;
+        font-weight: normal;
+        letter-spacing: .2rem;
+    }
+`
 
 const CharacterPrint = (props) => {
     return (
-        <div className="container">
+        <StyledCharacterPrint>
             <h3>{props.character.name}</h3>
-        </div>
+        </StyledCharacterPrint>
     )
 }
 
